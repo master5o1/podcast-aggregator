68eafe152fbc817a714fb20dcc34a0bc00064baf
Jason Schwarzenberger <jason@js.org.nz>
2018-11-25 23:47:38 +1300

    multi podcast fetcher.

--- fetcher.js
+++ fetcher.js
@@ -15,54 +15,64 @@ const options = commandLineArgs(optionDefinitions);
 
 mongoose.connect(options.mongo || 'mongodb://localhost:27017/podcasts');
 
-const fetchPodcast = async id => {
-  try {
-    const podcast = await Podcast.findOne({ id })
-      .populate('data.episodes')
-      .exec();
-
-    if (!podcast) {
-      res.sendStatus(404);
-      return;
-    }
-
-    const xml = await fetchPodcastXml(podcast.url);
-    const json = await parsePodcastXml(xml);
-
-    const options = { upsert: true, new: true, setDefaultsOnInsert: true };
-    const episodes = await Promise.all(
-      json.episodes.map(async episode => {
-        return await Episode.findOneAndUpdate({ 'enclosure.url': episode.enclosure.url }, episode, options);
-      })
-    );
-
-    podcast.data = {
-      ...podcast.data,
-      ...json,
-      episodes: [...podcast.data.episodes, ...episodes]
-    };
-
-    podcast.save();
-
-    return {
-      id: podcast.id,
-      fetched: episodes.length,
-      total: podcast.data.episodes.length
-    };
-  } catch (e) {
-    console.error(e);
+const fetchPodcast = async podcast => {
+  if (!podcast) {
+    res.sendStatus(404);
+    return;
   }
+
+  const xml = await fetchPodcastXml(podcast.url);
+  const json = await parsePodcastXml(xml);
+
+  const options = { upsert: true, new: true, setDefaultsOnInsert: true };
+  const episodes = await Promise.all(
+    json.episodes.map(async episode => {
+      return await Episode.findOneAndUpdate({ 'enclosure.url': episode.enclosure.url }, episode, options);
+    })
+  );
+
+  podcast.data = {
+    ...podcast.data,
+    ...json,
+    episodes: [...podcast.data.episodes, ...episodes]
+  };
+
+  podcast.save();
+
+  return {
+    id: podcast.id,
+    fetched: episodes.length,
+    total: podcast.data.episodes.length
+  };
 };
 
 const init = async options => {
   if (options.id) {
-    console.log(`fetching one podcast ${options.id}`);
-    const done = await fetchPodcast(options.id);
-    console.log(done);
+    const podcast = await Podcast.findOne({ id: options.id })
+      .populate('data.episodes')
+      .exec();
 
+    console.log(`fetching podcast ${podcast.id}: ${podcast.url}`);
+    const done = await fetchPodcast(podcast);
+    console.log(done);
     return;
   }
-  console.log('not done anything');
+  console.log('fetching list of podcasts');
+  const podcasts = await Podcast.find()
+    .populate('data.episodes')
+    .exec();
+
+  for (let podcast of podcasts) {
+    console.log(`fetching podcast ${podcast.id}: ${podcast.url}`);
+    const done = await fetchPodcast(podcast);
+    console.log(done);
+  }
+  console.log('finished.');
 };
 
-init(options).then(p => process.exit(0));
+init(options)
+  .then(p => process.exit(0))
+  .catch(e => {
+    console.error(e);
+    process.exit(0);
+  });
